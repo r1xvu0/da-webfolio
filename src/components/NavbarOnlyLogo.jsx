d75a7d98fde93b9b4a5513d7edda88975ea35d0f
r1xvu0 <53752966+r1xvu0@users.noreply.github.com>
2021-11-10 19:49:30 +0100

    Quick Fix

--- src/components/NavbarOnlyLogo.jsx
+++ src/components/NavbarOnlyLogo.jsx
@@ -14,7 +14,7 @@ export default function NavbarOnlyLogo() {
       w="full"
       overflowY="hidden"
     >
-      <chakra.div h="5rem" mx="auto" maxW="1200px">
+      <chakra.div h="10rem" mx="auto" maxW="1200px">
         <Flex
           w="full"
           h="full"
@@ -25,7 +25,7 @@ export default function NavbarOnlyLogo() {
           <Flex alignitems="center" align="center">
             <Link to="/">
               <HStack>
-                <Image w={24} h={24} src="r1xvu0logo.png" />
+                <Image w={64} h={64} src={"src\public\r1xvu0logo.png"} />
               </HStack>
             </Link>
           </Flex>
